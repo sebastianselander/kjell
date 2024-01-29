cbcea35b0ebde8e976a4cc2e19792924b1a8ae94
sebastianselander <sebastian.selander@gmail.com>
2024-01-30 00:32:21 +0100

    playing around

--- shell.c
+++ shell.c
@@ -4,7 +4,33 @@
 #define TRUE 1
 #define FALSE 0
 
-int main(int argc, char *argv[]) {
+size_t len(char *string) {
+    size_t i = 0;
+    while(string[i]) {
+        i++;
+    }
+    return i;
+}
+
+char *concat(char *first, char *second) {
+    size_t length = len(first) + len(second);
+    char* new = (char*) malloc(sizeof(char) * length + 1);
+    int i = 0;
+    while (first[i]) {
+        new[i] = first[i];
+        i++;
+    }
+    int j = 0;
+    while(second[j]) {
+        new[i] = second[j];
+        i++;
+        j++;
+    }
+    new[i] = 0;
+    return new;
+}
+
+int shell() {
     char *buf = NULL, *token;
     size_t count = 0;
     ssize_t nread;
@@ -13,7 +39,7 @@ int main(int argc, char *argv[]) {
     char **array;
 
     while (TRUE) {
-        write(STDOUT_FILENO, "CShell$ ", 9);
+        write(STDOUT_FILENO, "CShell > ", 7);
         nread = getline(&buf, &count, stdin);
         if (nread == -1) {
             perror("Exiting shell");
@@ -36,7 +62,7 @@ int main(int argc, char *argv[]) {
                 break;
             case 0:
                 if (execve(array[0], array, NULL) == -1) {
-                    perror("Failed to execute");
+                    perror(array[0]);
                     exit(97);
                 }
                 break;
@@ -48,4 +74,19 @@ int main(int argc, char *argv[]) {
     }
     free(buf);
     return 0;
+}
+
+int main(int argc, char *argv[]) {
+    char *left;
+    char *right;
+    right = "bello";
+    size_t count;
+    int nread = getline(&left, &count, stdin);
+    left[len(left) - 1] = 0;
+    char *test = concat(left, right);
+    printf("%s\n", test);
+    free(left);
+    printf("Freed left");
+    free(right);
+    printf("Freed right");
 }
