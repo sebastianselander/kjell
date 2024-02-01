bd45a210c0a5ca5caa4d16f6d477305609051c23
sebastianselander <sebastian.selander@gmail.com>
2024-02-02 00:01:20 +0100

    ready for next step

--- shell.c
+++ shell.c
@@ -2,6 +2,7 @@
 #include <string.h>
 
 #define bool char
+#define HOME "/home/sebastian"
 
 char *BUILT_INS = {"cd"};
 
@@ -11,32 +12,43 @@ bool is_cd(char *command) {
 
 void pwd() {
     char *cwd = getcwd(NULL, 0);
-    printf("Current path: %s\n", cwd);
+    printf("%s\n", cwd);
+    fflush(stdout);
     free(cwd);
 }
 
 void run_cd(char *command, char *arg_path, char **prev_path) {
+    *prev_path = getcwd(NULL, 0);
     int err = chdir(arg_path);
     if (err != 0) {
         perror("");
         exit(1);
-    } else {
-        *prev_path = getcwd(NULL, 0);
     }
 }
 
-char **tokenize(char *string, int *i) {
-    int j = *i;
+char **tokenize(char *string, int *size) {
     char *token = strtok(string, " \n");
     char **array = malloc(sizeof(char) * 1024);
     while (token) {
-        array[j] = token;
+        array[*size] = token;
         token = strtok(NULL, " \n");
-        j++;
+        *size = *size + 1;
     }
-    array[j] = NULL;
-    i = &j;
+    array[*size] = NULL;
     return array;
+}
+
+void printArray(char **array, int size) {
+    printf("{ ");
+    for (int i = 0; i < size; i++) {
+        printf("%s ", array[i]);
+    }
+    printf("} \n");
+}
+
+// returns 0 on success, -1 if command was not cd
+int execcd() {
+
 }
 
 int shell() {
@@ -44,50 +56,48 @@ int shell() {
     size_t count = 0;
     ssize_t nread;
     pid_t pid;
-    int status, i = 0;
-    char **array;
+    int status;
     char *cwd = getcwd(NULL, 0);
     char **prev_path = &cwd;
 
     while (1) {
         pwd();
-        fflush(stdout);
         write(STDOUT_FILENO, "CShell > ", 9);
-        fflush(stdout);
         nread = getline(&buf, &count, stdin);
         if (nread == -1) {
             perror("Exiting shell");
             exit(EXIT_FAILURE);
         }
-        array = tokenize(buf, &i);
-        /* token = strtok(buf, " \n"); */
-        /* array = malloc(sizeof(char*) * 1024); */
-        while (token) {
-            array[i] = token;
-            token = strtok(NULL, " \n");
-            i++;
-        }
-        array[i] = NULL;
+        int i = 0;
+        char **array = tokenize(buf, &i);
+        printArray(array, i);
         char *command = array[0];
         pid = fork();
-        if (pid == 0) {
-            if (is_cd(command)) {
-                if (i == 2) {
-                    char *argument = array[1];
-                    if (strcmp(argument, "~") == 0) {
-                        run_cd(command, "/home/sebastian", prev_path);
-                    } else if (strcmp(argument, "-") == 0) {
-                        run_cd(command, *prev_path, prev_path);
+        if (pid != -1) {
+            if (pid != 0) {
+                printf("pid [%d]\n", pid);
+            } else {
+                if (is_cd(command)) {
+                    if (i == 2) {
+                        char *argument = array[1];
+                        if (strcmp(argument, "~") == 0) {
+                            run_cd(command, HOME, prev_path);
+                        } else if (strcmp(argument, "-") == 0) {
+                            run_cd(command, *prev_path, prev_path);
+                        } else {
+                            run_cd(command, array[1], prev_path);
+                        }
+                    } else if (i == 1) {
+                        run_cd(command, HOME, prev_path);
                     } else {
-                        run_cd(command, array[1], prev_path);
+                        fprintf(stderr, "one argument required\n");
                     }
                 } else {
-                    run_cd(command, "/home/sebastian", prev_path);
-                }
-            } else {
-                if (execve(command, array, NULL) == -1) {
-                    perror("");
-                    exit(127);
+                    array[0] = NULL; // Should be filename
+                    if (execve(command, array, NULL) == -1) {
+                        perror("");
+                        exit(127);
+                    }
                 }
             }
         }
