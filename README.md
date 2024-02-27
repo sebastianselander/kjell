ae479e6b705a33fafd4cd7718078dff792748f6e
sebastianselander <sebastian.selander@gmail.com>
2024-02-27 23:57:09 +0100

    add `nix run` possbility and updat readme

--- README.md
+++ README.md
@@ -1,5 +1,33 @@
-A basic shell written in C.
+A basic kjell written in C.
 
-Run: `make run`
+# Implemented
 
-Currently far from usable. Basically only a parser, and parsing bash sucks
++ Cool prompt
++ Fork/wait
++ Cd
++ Exit code negation (!)
++ Sequential lists
++ Or lists
++ And lists
++ Exec
+
+## Next
+
++ Subshell
++ and much more
+
+# Run
++ `nix run "github:sebastianselander/kjell"`
++ `make run` with all dependencies installed
+
+# Development
+
+`nix develop` to enter a development shell with all dependencies, then running
+the kjell should only require `make run`
+
+The following are the dependencies if access to nix flakes is not possible.
++ C compiler
++ Make
++ BNFC
++ Bison
++ Flex
--- flake.nix
+++ flake.nix
@@ -13,33 +13,38 @@
       "i686-linux"
       "aarch64-linux"
       "x86_64-darwin"
-    ] (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
+    ]
+      (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
 
-          overlays = [ ];
-        };
-      in {
-        devShells.default = pkgs.mkShell rec {
-          name = "Kjell shell";
+            overlays = [ ];
+          };
+        in
+        {
+          buildPhase = '' make run'';
+          devShells.default = pkgs.mkShell rec {
+            name = "Kjell shell";
 
-          packages = with pkgs; [
-            ccls
-            clang-tools
-            bison
-            haskellPackages.BNFC
-            flex
-          ];
+            packages = with pkgs; [
+              ccls
+              clang-tools
+              bison
+              haskellPackages.BNFC
+              flex
+            ];
 
-          # development.
-          shellHook = let icon = "f121";
-          in ''
-            export PS1="$(echo -e '\u${icon}') {\[$(tput sgr0)\]\[\033[38;5;228m\]\w\[$(tput sgr0)\]\[\033[38;5;15m\]} (${name}) \\$ \[$(tput sgr0)\]"
-          '';
-        };
+            # development.
+            shellHook =
+              let icon = "f121";
+              in ''
+                export PS1="$(echo -e '\u${icon}') {\[$(tput sgr0)\]\[\033[38;5;228m\]\w\[$(tput sgr0)\]\[\033[38;5;15m\]} (${name}) \\$ \[$(tput sgr0)\]"
+              '';
+          };
 
-        packages.default = pkgs.callPackage ./default.nix { };
-      });
+          # packages.default = pkgs.callPackage ./default.nix { };
+          packages.default = pkgs.writeScriptBin "kjell" ''make run'';
+        });
 }
 
