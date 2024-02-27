3211e30475715d82f6b9210220f87b5524ab5f05
sebastianselander <sebastian.selander@gmail.com>
2024-02-27 22:31:58 +0100

    Get home path from env

--- src/builtins.c
+++ src/builtins.c
@@ -4,7 +4,6 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-#define HOME_PATH "/home/sebastian"
 void kjell_cd(Shell *shell, ListIdentifierLen lil) {
     ExitInfo exit_info = exit_info_init();
     if (lil.list_len > 1) {
@@ -12,11 +11,10 @@ void kjell_cd(Shell *shell, ListIdentifierLen lil) {
         printf("cd: too many arguments\n");
         return;
     }
-    char *path = HOME_PATH;
+    char *path = getenv("HOME");
     if (lil.list_len > 0) {
         char *first_arg = lil.list->identifier_;
         if (strcmp("-", first_arg) == 0) {
-            printf("Previous: %s\n", shell->previous_path);
             path = shell->previous_path;
         } else {
             path = first_arg;
